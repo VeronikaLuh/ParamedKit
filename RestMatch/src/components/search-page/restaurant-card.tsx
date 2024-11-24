eea78c6f80e1c0ba60bbd097453594ec0d004b27
Bohdan Halian <103418846+Liloosh@users.noreply.github.com>
2024-11-24 16:54:15 +0200

    fix_search_page

--- RestMatch/src/components/search-page/restaurant-card.tsx
+++ RestMatch/src/components/search-page/restaurant-card.tsx
@@ -2,18 +2,18 @@ import { imageUrl } from "@/utils/constants";
 
 const RestaurantCard = () => {
   return (
-    <div className="bg-[#8d6539] rounded-[10px] flex items-center justify-between mb-14 px-[31px] pt-[34px] pb-[24px] gap-4 drop-shadow-2xl">
+    <div className="bg-[#8d6539] rounded-[10px] flex items-center justify-between mb-14 px-[31px] pt-[34px] pb-[24px] gap-4 drop-shadow-2xl min-w-[55.9rem]">
       <img
         className="rounded-lg w-[316px] h-[239px]"
         src={`${imageUrl}/searchpage/restourant-icon.jpg`}
         alt="Description of image"
       />
-      <div className="flex flex-col self-start text-white max-h-[239px]">
+      <div className="flex flex-col self-start gap-1 text-white max-h-[239px]">
         <h3 className="leading-none text-[36px] font-normal">
           Biblioteka resto•bar
         </h3>
         <h3 className="text-[32px]">Lviv • 150 ₴ • Italic</h3>
-        <p className="text-[20px]  overflow-hidden">
+        <p className="text-[20px] overflow-hidden">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget
           faucibus tortor. Vivamus blandit eros in enim mollis, vel lobortis
           neque eleifend. Sed a consectetur tellus, ut sodales nibh. Lorem ipsum
