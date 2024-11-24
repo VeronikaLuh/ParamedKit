9a37e1cdcac7d3e9a9f51aa87f67dcdd40cedf1e
Alberda Nazarii <nazaralberda7@gmail.com>
2024-11-24 21:07:52 +0200

    add more requests

--- RestMatch/src/app/api/restaurant/route.ts
+++ RestMatch/src/app/api/restaurant/route.ts
@@ -9,5 +9,39 @@ export async function GET(req: Request) {
     req: req
   });
 
-  return NextResponse.json({ message: 'Success!', data: response });
+  return NextResponse.json({ data: response });
+}
+
+export async function POST(req: Request) {
+  const response = await ApiService.makeHttpsRequest({
+    url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/Restaurants`,
+    method: HttpMethods.POST,
+    req: req
+  });
+
+  return NextResponse.json({ data: response });
+}
+
+export async function PUT(req: Request, { params }: { params: { id: string } }) {
+  const id = parseInt(params.id, 10);
+
+  const response = await ApiService.makeHttpsRequest({
+    url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/Restaurants/${id}`,
+    method: HttpMethods.PUT,
+    req: req
+  });
+
+  return NextResponse.json({ data: response });
+}
+
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+  const id = parseInt(params.id, 10);
+
+  const response = await ApiService.makeHttpsRequest({
+    url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/Restaurants/${id}`,
+    method: HttpMethods.DELETE,
+    req: req
+  });
+
+  return NextResponse.json({ data: response });
 }
--- RestMatch/src/services/restaurant.service.tsx
+++ RestMatch/src/services/restaurant.service.tsx
@@ -1,5 +1,6 @@
 import ApiService from "@/services/api.service";
 import {HttpMethods} from "@/types/enum";
+import {Restaurant} from "@/models/Restaurant";
 
 const getRestaurants = async () => {
   return await ApiService.makeApiRequest({
@@ -8,16 +9,34 @@ const getRestaurants = async () => {
   });
 }
 
-// const createOrder = async (order: Order) => {
-//   return await ApiService.makeApiRequest({
-//     url: '/api/order',
-//     method: HttpMethods.POST,
-//     body: order
-//   });
-// }
+const addRestaurant = async (restaurant: Restaurant) => {
+  return await ApiService.makeApiRequest({
+    url: '/api/restaurant',
+    method: HttpMethods.POST,
+    body: restaurant
+  });
+}
+
+const updateRestaurant = async (restaurant: Restaurant, id: number) => {
+  return await ApiService.makeApiRequest({
+    url: `/api/restaurant/${id}`,
+    method: HttpMethods.PUT,
+    body: restaurant
+  });
+}
+
+const deleteRestaurant = async (id: number) => {
+  return await ApiService.makeApiRequest({
+    url: `/api/restaurant/${id}`,
+    method: HttpMethods.DELETE
+  });
+}
 
 const restaurantService = {
-  getRestaurants
+  getRestaurants,
+  addRestaurant,
+  updateRestaurant,
+  deleteRestaurant
 }
 
 export default restaurantService;
