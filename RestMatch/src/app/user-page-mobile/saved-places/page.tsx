a721857e67140c85e8a383bb3e11d8798cc89078
VERONlKA <119490634+VeronikaLuh@users.noreply.github.com>
2024-11-29 01:46:03 +0200

    Add mobile adaptive screen for user page(preferences, change info, saved places

--- RestMatch/src/app/user-page-mobile/saved-places/page.tsx
+++ RestMatch/src/app/user-page-mobile/saved-places/page.tsx
@@ -3,10 +3,11 @@ import SavedPlaceCard from "@/components/userpage/saved-places/saved-place-card"
 const SavedPlaces = () => {
   return (
     <div className="w-full">
-      <div className="w-full pt-16 px-7">
-        <h1 className="px-[3.25rem] mb-10">My saved places</h1>
-        <div className="grid grid-cols-3 gap-y-10">
-          <SavedPlaceCard />
+      <div className="w-full pt-3 px-7">
+        <h1 className=" mb-5 text-[35px] font-bold">
+          My saved places
+        </h1>
+        <div className="grid grid-cols-1 gap-y-10">
           <SavedPlaceCard />
           <SavedPlaceCard />
           <SavedPlaceCard />
