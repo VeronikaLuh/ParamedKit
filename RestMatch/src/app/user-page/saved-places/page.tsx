64a36e766d3a1f71cd7585daa8a8ea08c433d5cc
Alberda Nazarii <nazaralberda7@gmail.com>
2024-11-30 23:00:08 +0200

    add displaying favourites for user page

--- RestMatch/src/app/user-page/saved-places/page.tsx
+++ RestMatch/src/app/user-page/saved-places/page.tsx
@@ -1,16 +1,30 @@
+'use client';
+
 import SavedPlaceCard from "@/components/userpage/saved-places/saved-place-card";
+import {useEffect, useState} from "react";
+import {favouritesService} from "@/services/favourites.service";
 
 const SavedPlaces = () => {
+  const [data, setDate] = useState([]);
+
+  const fetchFavourites = async () => {
+    const response = await favouritesService.getFavourites();
+    setDate(response.data);
+  }
+
+  useEffect(() => {
+    fetchFavourites();
+  }, []);
+
   return (
     <div className="w-full">
       <div className="w-full pt-16 px-7">
         <h1 className="px-[3.25rem] mb-5">My saved places</h1>
         <div className="grid grid-cols-3 gap-y-10">
-          <SavedPlaceCard />
-          <SavedPlaceCard />
-          <SavedPlaceCard />
-          <SavedPlaceCard />
-          <SavedPlaceCard />
+          {data && data.map((item: any, index) => (
+            <SavedPlaceCard key={index} title={item.restaurant.name} />
+          ))}
+
         </div>
       </div>
     </div>
