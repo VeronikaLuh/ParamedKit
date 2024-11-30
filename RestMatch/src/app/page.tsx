1edb6c6cb458f7e774958c65cf1d9305d429a497
VERONlKA <119490634+VeronikaLuh@users.noreply.github.com>
2024-11-30 23:13:35 +0200

    Fix main page

--- RestMatch/src/app/page.tsx
+++ RestMatch/src/app/page.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client'
 
-import { imageUrl } from "@/utils/constants";
+import {imageUrl} from "@/utils/constants";
 import InfoItem from "@/components/InfoItem";
 import React, { useState } from "react";
 import { useSwipeable } from "react-swipeable";
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
 import { Routes } from "@/types/routes";
 import "@splidejs/splide/css";
 // @ts-ignore
@@ -15,34 +15,35 @@ export default function Home() {
     {
       title: "Register or login",
       description:
-        "Sign up or log in to personalize your experience and start discovering the best restaurants tailored to your tastes!",
+        "Quick way to choose where you must go today. Delicious and beautiful places await you",
       enableButton: true,
       buttonText: "Register",
-      url: Routes.SIGN_UP,
+      url: Routes.SIGN_UP
     },
     {
-      title: "Set preferences",
+      title: "Choose preferences",
       description:
-        "Set your preferences easily with our interactive selection to get restaurant recommendations that match your style!",
+        "Quick way to choose where you must go today. Delicious and beautiful places await you",
       enableButton: true,
       buttonText: "Make choice",
-      url: Routes.PREFERENCES,
+      url: Routes.PREFERENCES
     },
     {
       title: "Like or pass",
       description:
-        "Use our fun and interactive swipe feature to quickly choose your next dining spot based on smart recommendations!",
+        "Quick way to choose where you must go today. Delicious and beautiful places await you",
       enableButton: true,
       buttonText: "Let's match",
-      url: Routes.MATCH,
+      url: Routes.MATCH
+
     },
     {
       title: "Don`t like swipe?",
       description:
-        "Find your perfect restaurant by filtering results based on your specific needs and desires!",
+        "Quick way to choose where you must go today. Delicious and beautiful places await you",
       enableButton: true,
       buttonText: "Search",
-      url: Routes.SEARCH,
+      url: Routes.SEARCH
     },
   ];
 
@@ -69,22 +70,41 @@ export default function Home() {
 
   const router = useRouter();
   const handleRoute = (url: Routes) => {
-    router.push(url);
-  };
+    router.push(url)
+  }
 
   return (
     <main>
-      <div className='flex h-[582px] px-6'>
-        <img className='absolute w-full top-0 left-0 right-0 h-[582px] ' src={`${imageUrl}/homepage/home-background.png`} alt="coffee"/>
-        <div className='z-10 flex justify-between text-[40px] w-full mt-[80px] font-medium'>
-          <h2 className='text-left max-w-[521px]'>Find the best place to hangout with friends or celebrate important event</h2>
-          <h2 className='text-right max-w-[500px]'>Quick way to choose where you must go today. Delicious and beautiful places await you</h2>
+      <div className="flex px-6 h-[460px] md:h-[582.5px]">
+        <img
+          className="hidden md:block absolute w-full top-0 left-0 right-0 h-[582px]"
+          src={`${imageUrl}/homepage/home-background.png`}
+          alt="coffee"
+        />
+        <img
+          className="absolute w-full top-0 left-0 right-0 h-[520px] rounded-b-[50px] md:hidden "
+          src={`${imageUrl}/homepage/main_sm3.jpg`}
+          alt="coffee"
+        />
+        <div className="z-10 mt-7 flex justify-between w-full font-medium md:mt-[80px]">
+          <h2 className="hidden md:block max-w-[521px] text-left">
+            Find the best place to hangout with friends or celebrate important
+            event
+          </h2>
+          <h2 className="hidden md:block max-w-[500px] text-right">
+            Quick way to choose where you must go today. Delicious and beautiful
+            places await you
+          </h2>
+          <h2 className="max-w-[180px] md:hidden">
+            Quick way to choose where you must go today. Delicious and beautiful
+            places await you
+          </h2>
         </div>
       </div>
-      <div className='pt-[55px] px-6'>
-        <h2 className='font-bold'>What you need?</h2>
-        <hr/>
-        <div className='grid gap-2 mt-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+      <div className="sm: pt-[20px] px-6 lg:px-6">
+        <h2 className="font-bold">What you need?</h2>
+        <hr />
+        <div className="hidden md:grid gap-2 mt-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {data.map((item, index) => (
             <InfoItem
               key={index}
@@ -96,29 +116,77 @@ export default function Home() {
           ))}
         </div>
       </div>
-      <div className="pt-6 md:hidden md:pt-0">
-        <Splide
-          options={{
-            rewind: true,
-            gap: "1rem",
-            type: "loop",
-            padding: "2.3rem",
-            drag: "free",
-            snap: true,
+      <div className=" md:hidden">
+        <div className="pt-6 md:hidden md:pt-0">
+          <Splide
+            options={{
+              rewind: true,
+              gap: "1rem",
+              type: "loop",
+              padding: "2.3rem",
+              drag: "free",
+              snap: true,
+            }}
+            aria-label="Dynamic Items Slider"
+          >
+            {data.map((item, index) => (
+              <SplideSlide key={index}>
+                <InfoItem
+                  title={item.title}
+                  description={item.description}
+                  buttonText={item.buttonText}
+                  onClickHandle={() => handleRoute(item.url)}
+                />
+              </SplideSlide>
+            ))}
+          </Splide>
+        </div>
+      </div>
+      <div
+        {...swipeHandlers}
+        className="hidden relative w-full max-w-md mx-auto overflow-hidden"
+      >
+        <div
+          className="flex transition-transform duration-500 ease-in-out"
+          style={{
+            transform: `translateX(-${currentIndex * 100}%)`,
+            width: `${data.length * 100}%`, // Робимо контейнер ширшим для всіх слайдів
           }}
-          aria-label="Dynamic Items Slider"
         >
           {data.map((item, index) => (
-            <SplideSlide key={index}>
-              <InfoItem
-                title={item.title}
-                description={item.description}
-                buttonText={item.buttonText}
-                onClickHandle={() => handleRoute(item.url)}
-              />
-            </SplideSlide>
+            <div
+              key={index}
+              style={{
+                flexBasis: "100%",
+              }}
+              className={`flex-none w-full h-[400px]carousel-item transition-transform duration-500 ease-in-out transform `}
+            >
+              <div className="grid gap-4 mt-6 grid-cols-1">
+                <InfoItem
+                  key={index}
+                  title={item.title}
+                  description={item.description}
+                  buttonText={item.buttonText}
+                  onClickHandle={() => handleRoute(item.url)}
+                />
+              </div>
+            </div>
           ))}
-        </Splide>
+        </div>
+
+        {/* Управління */}
+        <button
+          className="absolute top-1/2 -translate-y-1/2 left-0 bg-white text-black p-2 rounded-full shadow-lg"
+          onClick={handlePrev}
+        >
+          ❮
+        </button>
+        <button
+          className="absolute top-1/2 -translate-y-1/2 right-0 bg-white text-black p-2 rounded-full shadow-lg"
+          onClick={handleNext}
+        >
+          ❯
+        </button>
       </div>
       <div className="pt-[80px] pb-[44px] pl-6 pr-6 lg:pr-0">
         <div className="flex relative">
@@ -127,21 +195,23 @@ export default function Home() {
               Want to see place that you liked earlier?
             </h2>
             <p className="leading-9 text-[1.6rem] md:text-[1.875rem]">
-              Found a restaurant you love? Save it to your "My Saved Places" tab
-              with just a tap! This way, you can easily revisit your favorite
-              spots or explore them later at your convenience. Never worry about
-              forgetting the name of that perfect place again!
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic,
+              itaque saepe? Ab aliquam dicta, dolores ea enim eveniet,
+              exercitationem facilis fugiat impedit iusto nesciunt quibusdam
+              quis rem sint ut vel. Alias asperiores assumenda aut blanditiis
+              consequatur consequuntur distinctio doloribus harum ipsa maxime,
+              porro quidem ratione repudiandae ullam ut, vero, vitae? Animi
             </p>
-
             <button className="mt-[41px] bg-[#5D462D] py-2 px-9 rounded-[40px] font-bold text-[1.5rem] w-left md:w-fit text-[2rem]">
               My saved
             </button>
           </div>
-          <div className='xl:flex-[1] hidden lg:flex justify-end '>
+          <div className="xl:flex-[1] hidden lg:flex justify-end ">
             <img
               src={`${imageUrl}/homepage/coffee-hand.png`}
-              className='h-[280px] w-[315px] xl:h-fit xl:w-auto'
-              alt="hand with coffe"/>
+              className="h-[280px] w-[315px] xl:h-fit xl:w-auto"
+              alt="hand with coffe"
+            />
           </div>
         </div>
       </div>
